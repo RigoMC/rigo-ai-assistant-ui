c58b480d39d94adb4488268ad37bf7889155137b
Rigoberto Mancera <34317240+RigoMC@users.noreply.github.com>
2025-08-10 21:52:20 -0600

    Update layout.tsx

--- src/app/api/auth/signin/layout.tsx
+++ src/app/api/auth/signin/layout.tsx
@@ -1,6 +1,5 @@
 import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
-import {SessionProvider} from "next-auth/react";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
